4f9ac556d5efd0fcabacd77d8a38de1516656887
Despo <tornike.despotashvili@gmail.com>
2024-10-06 15:07:05 +0400

    create swit proj

---
+++ module-2/Assigmenet11/Assigmenet1/Assigmenet1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assigmenet1
+//
+//  Created by Despo on 06.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
