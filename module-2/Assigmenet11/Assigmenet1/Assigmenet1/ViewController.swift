a8aadb40a1db6f2fe12d6445250868b896446ed9
Despo <tornike.despotashvili@gmail.com>
2024-10-07 13:05:09 +0400

    finish

--- module-2/Assigmenet11/Assigmenet1/Assigmenet1/ViewController.swift
+++ module-2/Assigmenet11/Assigmenet1/Assigmenet1/ViewController.swift
@@ -7,13 +7,118 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel?
+    @IBOutlet weak var firstField: UITextField?
+    @IBOutlet weak var secondField: UITextField?
+    @IBOutlet weak var result: UILabel?
+    @IBOutlet weak var themeIcon: UIImageView?
+    
+    var dividerMode = false
+    var isDarkMode = false
+    var firstNumber = 0
+    var secondNumber = 0
+    var finalResult = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
-
+    
+    
+    @IBAction func changdeDividerMode() {
+        dividerMode = !dividerMode
+        changeTitleLabel()
+    }
+    
+    @IBAction func setFirstNum() {
+        let fieldValue = firstField?.text ?? ""
+        
+        if checkRegex(input: fieldValue) {
+            firstNumber = Int(fieldValue) ?? 0
+        }
+    }
+    
+    @IBAction func setSecondNum() {
+        let fieldValue = secondField?.text ?? ""
+        
+        if checkRegex(input: fieldValue) {
+            secondNumber = Int(fieldValue) ?? 0
+        }
+    }
+    
+    
+    private func setupUI() {
+        changdeDividerMode()
+        setupGesture()
+        changeTheme()
+        
+        switch traitCollection.userInterfaceStyle {
+        case .light:
+            isDarkMode = false
+        case .dark:
+            isDarkMode = true
+        default:
+            isDarkMode = false
+        }
+    }
+    
+    private func setupGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(changeTheme))
+        
+        themeIcon?.isUserInteractionEnabled = true
+        themeIcon?.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func changeTheme() {
+        isDarkMode = !isDarkMode
+        
+        if isDarkMode {
+            themeIcon?.image = UIImage(systemName: "sun.max.fill")
+            themeIcon?.tintColor = .yellow
+            
+            firstField?.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
+            secondField?.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
+            
+            overrideUserInterfaceStyle = .dark
+        } else {
+            themeIcon?.tintColor = .systemBlue
+            themeIcon?.image = UIImage(systemName: "moon.fill")
+            
+            firstField?.backgroundColor = UIColor.clear
+            secondField?.backgroundColor = UIColor.clear
+            
+            overrideUserInterfaceStyle = .light
+        }
+    }
+    
+    private func checkRegex(input: String) -> Bool {
+        let keyAndValue = "^[1-9]+$"
+        
+        if input.range(of: keyAndValue, options: .regularExpression) != nil {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    private func changeTitleLabel() {
+        titleLabel?.text = dividerMode ? "ნაშთიანი გაყოფა" : "უნაშთო გაყოფა"
+    }
+    
+    @IBAction func calculate() {
+        if secondNumber == 0 {
+            result?.text = "ნულზე გაყოფა არ შეიძლება"
+        } else {
+            if dividerMode {
+                finalResult = firstNumber % secondNumber
+            } else {
+                finalResult = firstNumber / secondNumber
+            }
+            
+            result?.text = String(finalResult)
+        }
+    }
 }
 
