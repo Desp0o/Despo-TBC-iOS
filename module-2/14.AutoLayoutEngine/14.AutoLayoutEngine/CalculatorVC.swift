89b965298a1feafc2a48e22bf6cf27f4f16e7a48
Despo <tornike.despotashvili@gmail.com>
2024-10-13 19:56:58 +0400

    project init

---
+++ module-2/14.AutoLayoutEngine/14.AutoLayoutEngine/CalculatorVC.swift
@@ -0,0 +1,15 @@
+//
+//  CalculatorVC.swift
+//  14.AutoLayoutEngine
+//
+//  Created by Despo on 13.10.24.
+//
+
+import UIKit
+
+class CalculatorVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
