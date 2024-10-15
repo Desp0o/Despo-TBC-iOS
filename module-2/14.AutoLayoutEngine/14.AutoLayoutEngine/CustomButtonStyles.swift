46685af108349b6d9a41318bad5d1f3dc19de047
Despo <tornike.despotashvili@gmail.com>
2024-10-15 18:59:18 +0400

    update

--- module-2/14.AutoLayoutEngine/14.AutoLayoutEngine/CustomButtonStyles.swift
+++ module-2/14.AutoLayoutEngine/14.AutoLayoutEngine/CustomButtonStyles.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class CalcButton: UIButton {
-        
+    
     init() {
         super.init(frame: .zero)
         buttonCornerRadius()
@@ -20,10 +20,24 @@ final class CalcButton: UIButton {
     
     private func buttonCornerRadius() {
         self.clipsToBounds = true
-        self.layer.cornerRadius = self.frame.width / 2
         self.translatesAutoresizingMaskIntoConstraints = false
-    }
+        NSLayoutConstraint.deactivate(self.constraints)
         
+        if UIScreen.main.bounds.height > UIScreen.main.bounds.width {
+            self.layer.cornerRadius = self.frame.width / 2
+            
+            NSLayoutConstraint.activate([
+                self.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1),
+            ])
+        } else {
+            self.layer.cornerRadius = 16
+            
+            NSLayoutConstraint.activate([
+                self.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.25),
+            ])
+        }
+    }
+    
     func setupButtonsUI(name: String = "", isDarkMode: Bool = false, isFilled: Bool = false, isBordered: Bool = false, iconLight: String = "", iconDark: String = "") {
         let darkModeColor = UIColor(hue: 220/360, saturation: 0.11, brightness: 0.22, alpha: 1).cgColor
         let lightModeColor = UIColor(hue: 0, saturation: 0, brightness: 0.91, alpha: 1).cgColor
@@ -32,16 +46,16 @@ final class CalcButton: UIButton {
             self.setTitle(name, for: .normal)
             self.titleLabel?.font = UIFont.systemFont(ofSize: 24)
         }
-
+        
         if isFilled {
             self.layer.backgroundColor = isDarkMode ? darkModeColor : lightModeColor
         }
-
+        
         if isBordered {
             self.layer.borderWidth = 2
             self.layer.borderColor = isDarkMode ? darkModeColor : lightModeColor
         }
-
+        
         if !iconDark.isEmpty {
             self.setImage(UIImage(named: isDarkMode ? iconLight : iconDark), for: .normal)
         }
@@ -50,7 +64,7 @@ final class CalcButton: UIButton {
     func updatetextColor(isDarkMode: Bool) {
         self.setTitleColor(isDarkMode ? .white : .black, for: .normal)
     }
-
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         buttonCornerRadius()
