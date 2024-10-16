a6b21abf575b3731b60c3a302d0a668d7eb96853
Despo <tornike.despotashvili@gmail.com>
2024-10-16 21:25:13 +0400

    fin..

--- module-2/14.AutoLayoutEngine/14.AutoLayoutEngine/HistoryVC.swift
+++ module-2/14.AutoLayoutEngine/14.AutoLayoutEngine/HistoryVC.swift
@@ -1,51 +1,79 @@
-//
-//  HistoryVCViewController.swift
-//  14.AutoLayoutEngine
-//
-//  Created by Despo on 16.10.24.
-//
-
 import UIKit
 
 class HistoryVC: UIViewController {
+    private let backButton = UIButton()
+    private let mainView = UIStackView()
     
-    let isDarkMode: Bool
-    
-    init(isDarkMode: Bool) {
+    var isDarkMode: Bool
+    var operationsArray: [String]
+
+    init(isDarkMode: Bool, operationsArray: [String]) {
         self.isDarkMode = isDarkMode
+        self.operationsArray = operationsArray
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = isDarkMode ? darkThemeBackgroundColor : lightThemeBackgroundColor
-        // Do any additional setup after loading the view.
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupUI() {
+        view.backgroundColor = isDarkMode ? darkThemeBackgroundColor : lightThemeBackgroundColor
+        setupScreen()
+        setupHistoryLabels()
+        setupBackButton()
     }
-    */
+    
+    private func setupBackButton() {
+        view.addSubview(backButton)
+        view.bringSubviewToFront(backButton)
+        
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.setImage(UIImage(named: isDarkMode ? "backArrowLight" : "backArrowDark"), for: .normal)
+                
+        NSLayoutConstraint.activate([
+            backButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            backButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
+        ])
+        
+        backButton.addAction(UIAction(handler: {[weak self] action in
+            self?.navigationController?.popViewController(animated: true)
+        }), for: .touchUpInside)
+    }
+    
+    private func setupScreen() {
+        view.addSubview(mainView)
+        
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        mainView.axis = .vertical
+        mainView.distribution = .fill
+        mainView.spacing = 16
+        mainView.alignment = .trailing
+        
+        NSLayoutConstraint.activate([
+            mainView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            mainView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            mainView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            mainView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0)
+        ])
+    }
 
+    private func setupHistoryLabels() {
+        for label in operationsArray {
+            let lbl = UILabel()
+            lbl.text = label
+            lbl.textColor = isDarkMode ? .white : .black
+            lbl.font = UIFont.systemFont(ofSize: 20)
+            
+            mainView.addArrangedSubview(lbl)
+            
+            lbl.translatesAutoresizingMaskIntoConstraints = false
+            lbl.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: -29).isActive = true
+        }
+    }
 }
-
-
-
-
-//
-//
-//#Preview {
-//    let vc = HistoryVC(isDarkMode: false)
-//    return vc
-//}
