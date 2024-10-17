cd79712d5a7f5fe742cdf350c6473544d6b726ec
Despo <tornike.despotashvili@gmail.com>
2024-10-18 03:42:53 +0400

    start controller page

--- module-2/17. UITableView/17. UITableView/PlanetCell.swift
+++ module-2/17. UITableView/17. UITableView/PlanetCell.swift
@@ -9,20 +9,35 @@ import UIKit
 final class PlanetCell: UITableViewCell {
     private let cellStack = UIStackView()
     private let infoStack = UIStackView()
-    private let planetImg = UIImageView()
-    private let planetTitle = UILabel()
-    private let infoText = UILabel()
+    private var planetImg = UIImageView()
+    private var planetTitle = UILabel()
+    private var planetArea = UILabel()
+    private var arrowIocn = UIImageView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.backgroundColor = .clear
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    private func setupUI() {
+        setupMainStack()
+        setupImage()
+        setupInfoStack()
+        setupPlanetInfo()
+        setupArrowIcon()
     }
     
     private func setupImage() {
-        planetImg.translatesAutoresizingMaskIntoConstraints = false
-        
         NSLayoutConstraint.activate([
             planetImg.widthAnchor.constraint(equalToConstant: 100),
-            planetImg.heightAnchor.constraint(equalToConstant: 100)
+            planetImg.heightAnchor.constraint(equalToConstant: 100),
         ])
     }
     
@@ -31,15 +46,15 @@ final class PlanetCell: UITableViewCell {
         cellStack.addArrangedSubview(planetImg)
         
         cellStack.axis = .horizontal
+        cellStack.alignment = .center
         cellStack.spacing = 36
-        cellStack.alignment = .leading
         
         cellStack.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            cellStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            cellStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             cellStack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
-            cellStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+            cellStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             cellStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0)
         ])
     }
@@ -47,16 +62,36 @@ final class PlanetCell: UITableViewCell {
     private func setupInfoStack() {
         cellStack.addArrangedSubview(infoStack)
         infoStack.addArrangedSubview(planetTitle)
-        infoStack.addArrangedSubview(infoText)
+        infoStack.addArrangedSubview(planetArea)
+        
+        infoStack.axis = .vertical
+        infoStack.alignment = .top
         
         infoStack.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    private func setupPlanetCell(planetImage: UIImage, planetTitle: String, planetInfo: String) {
+        
+        NSLayoutConstraint.activate([
+            infoStack.leftAnchor.constraint(equalTo: planetImg.rightAnchor, constant: 36),
+            infoStack.topAnchor.constraint(equalTo: cellStack.topAnchor),
+            infoStack.bottomAnchor.constraint(equalTo: cellStack.bottomAnchor)
+        ])
         
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    private func setupPlanetInfo() {
+        planetTitle.font = UIFont.systemFont(ofSize: 36, weight: .bold)
+        
+        planetArea.font = UIFont.systemFont(ofSize: 18)
+    }
+    
+    private func setupArrowIcon() {
+        cellStack.addArrangedSubview(arrowIocn)
+        
+        arrowIocn.image = UIImage(named: "arrowIcon")
+    }
+    
+    func setupPlanetCell(planetImage: UIImage, planetTitle: String, planetArea: String) {
+        self.planetImg.image = planetImage
+        self.planetTitle.text = planetTitle
+        self.planetArea.text = planetArea
     }
 }
--- module-2/17. UITableView/17. UITableView/ViewController.swift
+++ module-2/17. UITableView/17. UITableView/ViewController.swift
@@ -7,14 +7,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    private var pageTitle = UILabel()
+    
+    private let planetsArray = [
+        ("Mars", "1,258,250 km²", UIImage(named: "mars")),
+        ("Jupiter", "64E10 km²", UIImage(named: "jupiter")),
+        ("Earth", "500,100,100 km²", UIImage(named: "earth")),
+        ("Saturn", "2,608,250 km²", UIImage(named: "saturn")),
+        ("Venus", "460,234,300 km²", UIImage(named: "venus")),
+        ("Mercury", "74,800,000 km²", UIImage(named: "mercury")),
+        ("Neptune", "7,618,300,000 km²", UIImage(named: "neptune")),
+        ("Uranus", "8,100,400,000 km²", UIImage(named: "uranus")),
+        ("Pluto", "1,654,500 km²", UIImage(named: "pluto")),
+        ("Eris", "1,663,000 km²", UIImage(named: "eris")),
+        ("Haumea", "1,949,000 km²", UIImage(named: "haumea")),
+        ("Makemake", "1,430,000 km²", UIImage(named: "makemake")),
+        ("Ceres", "2,770,000 km²", UIImage(named: "ceres"))
+    ]
+    
+    
+    private let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .secondarySystemBackground
-        // Do any additional setup after loading the view.
+        view.backgroundColor = backgorundColor
+        setupPageTitle()
+        setupTableView()
     }
-
-
+    
+    private func setupPageTitle() {
+        view.addSubview(pageTitle)
+        
+        pageTitle.text = "Planets"
+        pageTitle.font = UIFont.boldSystemFont(ofSize: 36)
+        pageTitle.textColor = primaryColor
+        
+        pageTitle.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            pageTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            pageTitle.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ])
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(PlanetCell.self, forCellReuseIdentifier: "PlanetCell")
+        tableView.dataSource = self
+        tableView.backgroundColor = .clear
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: pageTitle.topAnchor, constant: 69),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        planetsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlanetCell", for: indexPath) as! PlanetCell
+        let planet = planetsArray[indexPath.row]
+        cell.setupPlanetCell(planetImage: planet.2 ?? UIImage(), planetTitle: planet.0, planetArea: planet.1)
+        return cell
+    }
 }
-
