66aff37e5382a2228f7a5c1e86c8f781e0abd9b3
Despo <tornike.despotashvili@gmail.com>
2024-11-10 23:01:31 +0400

    implement webservice package

--- module-2/23. Modularization/19. web services/Feed/ViewModel.swift
+++ module-2/23. Modularization/19. web services/Feed/ViewModel.swift
@@ -5,25 +5,20 @@
 //  Created by Despo on 30.10.24.
 //
 import Foundation
+import NetworkManagerFramework
 
 protocol UpdateNewsDelegate: AnyObject {
     func updateNewsFeed()
 }
 
 final class ViewModel {
+    private let networkService: NetworkServiceProtocol
     weak var delegate: UpdateNewsDelegate?
     private var currentPage = 0
     
-    private var linkApi =  "https://newsapi.org/v2/everything?q=bitcoin&pageSize=30&page=\(1)&apiKey=815bdd179bed438aa183f4d2a6ff264f"
-    
-    
-    init() {
-//        loadNextPage()
-        
-        fetchData(url: linkApi) {[weak self] data, error in
-            self?.newsArray.append(contentsOf: data?.articles ?? [])
-            print(self?.newsArray)
-        }
+    init(networkService: NetworkServiceProtocol = NetworkService()) {
+        self.networkService = networkService
+        loadNextPage()
     }
     
     var newsArray: [SinglePost] = []
@@ -33,76 +28,27 @@ final class ViewModel {
     }
     
     func singlePost(index: Int) -> SinglePost {
-        if newsArray[index].title == "[Removed]" {
+        if index < newsArray.count && newsArray[index].title == "[Removed]" {
             newsArray.remove(at: index)
+            print("remove")
         }
-        
+            
         return newsArray[index]
-    }
-    
-    func fetchNews(page: Int) {
-        let urlString = "https://newsapi.org/v2/everything?q=bitcoin&pageSize=10&page=\(page)&apiKey=815bdd179bed438aa183f4d2a6ff264f"
-        let url = URL(string: urlString)
-        
-        guard let url = url else { return }
-        let urlRequest = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: urlRequest) {[weak self] data, response, error in
-            let decoder = JSONDecoder()
-            
-            if let error {
-                print(error)
-            }
-            
-            guard let response = response as? HTTPURLResponse else { return }
-            guard (200...299).contains(response.statusCode) else { return }
-            guard let data else { return }
-            
-            do {
-                let newsResponseData = try decoder.decode(NewsResponseData.self, from: data)
-                self?.newsArray.append(contentsOf: newsResponseData.articles)
-                
-                DispatchQueue.main.async {
-                    self?.delegate?.updateNewsFeed()
-                }
-            } catch {
-                print(error.localizedDescription)
-            }
-        }.resume()
-        
     }
     
     func loadNextPage() {
         currentPage += 1
-    }
-    
-    func fetchData(url: String, complition: @escaping (NewsResponseData?, Error?) -> Void) {
-        let url = URL(string: url)
         
-        guard let url = url else { return }
+        let linkApi =  "https://newsapi.org/v2/everything?q=bitcoin&pageSize=30&page=\(currentPage)&apiKey=815bdd179bed438aa183f4d2a6ff264f"
         
-        let urlRequest = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-            
-            if let error {
-                print(error)
+        networkService.fetchData(urlString: linkApi) { [weak self] (result: Result<NewsResponseData, NetworkError>) in
+            switch result {
+            case .success(let posts):
+                self?.newsArray.append(contentsOf: posts.articles)
+                self?.delegate?.updateNewsFeed()
+            case .failure(let error):
+                print("Error fetching data: \(error)")
             }
-            
-            guard let response = response as? HTTPURLResponse else { return }
-            guard (200...299).contains(response.statusCode) else { return }
-            guard let data else { return }
-            
-            do {
-                let fetchedData = try JSONDecoder().decode(NewsResponseData.self, from: data)
-                DispatchQueue.main.async {
-                    complition(fetchedData, nil)
-                    self.delegate?.updateNewsFeed()
-                }
-            } catch {
-                complition(nil, error.localizedDescription as! Error)
-            }
-            
-        }.resume()
+        }
     }
 }
