d8e4e13b528b32bc72f901f9c23cc5194c35990f
Desp0o <tornike.despotashvili@gmail.com>
2024-11-15 01:25:42 +0400

    setup monkey

--- module-2/25. Animations/25. Animations/ViewController.swift
+++ module-2/25. Animations/25. Animations/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let banana = UIImageView()
+    private let banana = UIImageView()
+    private let monkey = UIImageView()
+    private var initialCenter: CGPoint = .zero
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +18,7 @@ class ViewController: UIViewController {
         
         setupBanana()
         animateBanana()
+        setupMonkey()
     }
     
     private func setupBanana() {
@@ -34,23 +37,70 @@ class ViewController: UIViewController {
     }
     
     private func animateBanana() {
-            setupBananaPosition()
-            
-            UIView.animate(withDuration: 2.0,
-                          delay: 0,
-                          options: .curveLinear,
-                          animations: {
-                self.banana.frame.origin.y = self.view.bounds.height + 50
-            }) { completed in
-                if completed {
-                    self.animateBanana()
-                }
+        setupBananaPosition()
+        
+        UIView.animate(withDuration: 2.0,
+                       delay: 0,
+                       options: .curveLinear,
+                       animations: {
+            self.banana.frame.origin.y = self.view.bounds.height + 50
+        }) { completed in
+            if completed {
+                self.animateBanana()
             }
         }
+    }
     
-    func setupBananaPosition(){
-        let randomX = CGFloat.random(in: 0...(view.bounds.width - 20))
+    private func setupBananaPosition(){
+        let randomX = CGFloat.random(in: 0...(view.bounds.width - 50))
         banana.frame.origin = CGPoint(x: randomX, y: 50)
+    }
+    
+    private func setupMonkey() {
+        view.addSubview(monkey)
+        monkey.translatesAutoresizingMaskIntoConstraints = false
+        monkey.image = UIImage(named: "monkey")
+        monkey.contentMode = .scaleAspectFit
+        monkey.isUserInteractionEnabled = true
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        monkey.addGestureRecognizer(panGesture)
+        
+        NSLayoutConstraint.activate([
+            monkey.widthAnchor.constraint(equalToConstant: 100),
+            monkey.heightAnchor.constraint(equalToConstant: 100),
+            monkey.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            monkey.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+        ])
+    }
+    
+    @objc private func handlePan(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        
+        switch gesture.state {
+        case .began:
+            initialCenter = monkey.center
+            
+        case .changed:
+            var newCenter = CGPoint(
+                x: initialCenter.x + translation.x,
+                y: initialCenter.y + translation.y
+            )
+            
+            let halfWidth = monkey.bounds.width / 2
+            newCenter.x = min(max(newCenter.x, halfWidth), view.bounds.width - halfWidth)
+            
+            let halfHeight = monkey.bounds.height / 2
+            newCenter.y = min(max(newCenter.y, halfHeight), view.bounds.height - halfHeight)
+            
+            monkey.center = newCenter
+            
+        case .ended:
+            UIView.animate(withDuration: 0.3) { }
+            
+        default:
+            break
+        }
     }
 }
 
