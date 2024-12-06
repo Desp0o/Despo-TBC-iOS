c171b2f94a7b81458d26ba5841ccea8e2085ecc8
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2024-12-06 11:34:41 +0400

    new module|

---
+++ module-3/28. SwiftUI Basics/28. SwiftUI Basics/_8__SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8__SwiftUI_BasicsApp.swift
+//  28. SwiftUI Basics
+//
+//  Created by Despo on 06.12.24.
+//
+
+import SwiftUI
+
+@main
+struct _8__SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ module-3/28. SwiftUI Basics/28. SwiftUI BasicsUITests/_8__SwiftUI_BasicsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  _8__SwiftUI_BasicsUITestsLaunchTests.swift
+//  28. SwiftUI BasicsUITests
+//
+//  Created by Despo on 06.12.24.
+//
+
+import XCTest
+
+final class _8__SwiftUI_BasicsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
