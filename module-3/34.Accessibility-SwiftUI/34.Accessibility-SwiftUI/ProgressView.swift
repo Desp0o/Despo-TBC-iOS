e27471d145b732b1212064e6894c96277c365d71
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2024-12-25 04:03:18 +0400

    music st/st / ui

---
+++ module-3/34.Accessibility-SwiftUI/34.Accessibility-SwiftUI/ProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressView.swift
+//  34.Accessibility-SwiftUI
+//
+//  Created by Despo on 25.12.24.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+        HStack {
+            Rectangle()
+                .fill(.yellow)
+                .frame(width: 200, height: 100)
+        }
+    }
+}
